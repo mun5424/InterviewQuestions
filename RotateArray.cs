09516292f695dfc2bb386de9244372eeeaeba9ed
Charles Oh <mun5424@gmail.com>
2018-05-21 02:31:53 -0700

    rotate o(1) o(n)

--- RotateArray.cs
+++ RotateArray.cs
@@ -14,18 +14,61 @@
             if(k > array.Length)
                 k = k % array.Length; 
             int[] res = new int[array.Length]; 
+            
+            //copy from beginning of the array
             for(int i = 0; i < k; i++)
             {
                 res[i] = array[array.Length - k + i];
             }
             
+            //copy the rest
             int j = 0; 
             for(int i = k; i < array.Length; i++)
             {
                 res[i] = array[j];
                 j++;
             }
-            
+            return res;
 
+        }
+        public static int[] rotateArrayOptimal(int[] array, int k)
+        {
+            //solution 2 - reverse method
+            
+            //elegant and clever method to reverse array before k, after k, and then reverse them altogether.
+            if(k > array.Length)
+                k = k % array.Length; 
+            int[] res = new int[array.Length]; 
+            
+            //copy from beginning of the array
+            for(int i = 0; i < k; i++)
+            {
+                res[i] = array[array.Length - k + i];
+            }
+            
+            //copy the rest
+            int j = 0; 
+            for(int i = k; i < array.Length; i++)
+            {
+                res[i] = array[j];
+                j++;
+            }
+            return res;
+
+        }
+        public static int[] reverseArray(int[] array, int start, int end)
+        {
+            int left = start;
+            int right = end; 
+
+            while(left < right)
+            {
+                int temp = array[left]; 
+                array[left] = array[right];
+                array[right] = temp; 
+                left++;
+                right--; 
+            }
+            return array; 
         }
     }
