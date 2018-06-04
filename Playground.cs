b2f7f77a3a64bd1a220497215796465ef75401bd
Charles Oh <mun5424@gmail.com>
2018-06-03 22:08:00 -0700

    delete unused code

--- Playground.cs
+++ Playground.cs
@@ -9,22 +9,6 @@ namespace InterviewQuestions
 
     class Playground
     {
-        class UpdateRequest
-        {
-            public int ExternalId {get; protected set; }
-            public int ObjectId {get; protected set; }
-            public int VersionId {get; protected set; }
-
-        protected object CreateUpdateRequest<T>()
-        {
-            var updateRequest = new UpdateRequest();
-            updateRequest.ExternalId = 12;
-            updateRequest.ObjectId = 2;
-            updateRequest.VersionId = 1; 
-            return (object) updateRequest;
-
-        }
-        }
 
         
 
